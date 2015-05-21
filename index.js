05da59a53bf732681216d5ffb982c2da26c943a7
theeltahir <theeltahir@gmail.com>
2015-05-21 17:04:06 -0400

    added basic ajax

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser")
+
 
 var orm = require('./db.js');
 	orm.connect();
 var Row = orm.Row;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get("/:move?", function(req,res){
-	var move = req.params.move	
+	var move = req.params.move
 	if(move){
 		var data = move.split("_")
 		playSquare(data[0], data[1], data[2])
@@ -18,12 +23,26 @@ app.get("/:move?", function(req,res){
 
     res.render('index');
 });
+app.post('/move', function(req,res){
+  if(!req.body.user || !req.body.row || !req.body.col){
+    res.send("Error: [oid, name] are required.  Found: '" + Object.keys(req.body) + "'");
+    return;
+  }
+  playSquare(req.body.user,req.body.row, req.body.col, function(err,data){
+     if(err){
+      console.error('Error Saving', err);
+      res.send('Error saving');
+    }else{
+      res.send(data);
+    }
+  })
+});
 
 app.listen(3000, function(){
   console.log("listening on 3000");
   newGame();
 });
-	
+
 function newGame(){
     Row.collection.remove();
     for(x = 0; x < 8; x++){
@@ -31,9 +50,8 @@ function newGame(){
     };
 };
 
-function playSquare(user, row, col){
+function playSquare(user, row, col,callback){
 		var newData = {}
 		newData['space' + col] = user
-	    Row.update({row_id: row}, newData, function(doc){
-    });
+	    Row.update({row_id: row}, newData,callback);
 }
