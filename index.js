b5243317eb05f393d853eeb713021fecc30742d7
theeltahir <theeltahir@gmail.com>
2015-05-21 14:55:05 -0400

    adding new files

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/js"));
+app.use(express.static(__dirname + "/public/css"));
+
+app.get("/", function(req,res){
+   res.render('index');
+});
+
+app.listen(3000, function(){
+  console.log("listening on 3000");
+});
